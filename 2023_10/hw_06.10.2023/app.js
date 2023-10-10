aade548e35ad897fb7cf08d74e53a725c1dfb771
ZaliK007 <marshiktop505@gmail.com>
2023-10-10 20:17:40 +0500

    add task 1, 2, 3, 4, 5

--- 2023_10/hw_06.10.2023/app.js
+++ 2023_10/hw_06.10.2023/app.js
@@ -1,71 +1,70 @@
-// task1
-// let age = prompt('Введите возраст')
-// if (age <= 2){
-//    console.log('ребенок')
-// }
-// else if (age <= 18){
-//    console.log('подросток')
-// }
-// else if (age <= 60){
-//    console.log('взрослый')
-// }
-// else if (age > 60){
-//    console.log('пенсионер')
-// }
+let age = prompt('Введите возраст')
+if (age <= 2){
+   console.log('ребенок')
+}
+else if (age <= 18){
+   console.log('подросток')
+}
+else if (age <= 60){
+   console.log('взрослый')
+}
+else if (age > 60){
+   console.log('пенсионер')
+}
 
 
-// task2
-// let num = prompt('Введите число от 0 до 9')
-// if (num == 0){
-//     console.log(')')
-// }
-// else if (num == 1){
-//     console.log('!')
-// }
-// else if (num == 2){
-//     console.log('@')
-// }
-// else if (num == 3){
-//     console.log('#')
-// }
-// else if (num == 4){
-//     console.log('$')
-// }
-// else if (num == 5){
-//     console.log('%')
-// }
-// else if (num == 6){
-//     console.log('^')
-// }
-// else if (num == 7){
-//     console.log('&')
-// }
-// else if (num == 8){
-//     console.log('*')
-// }
-// else if (num == 9){
-//     console.log('(')
-// }
+
+let num = prompt('Введите число от 0 до 9')
+if (num == 0){
+    console.log(')')
+}
+else if (num == 1){
+    console.log('!')
+}
+else if (num == 2){
+    console.log('@')
+}
+else if (num == 3){
+    console.log('#')
+}
+else if (num == 4){
+    console.log('$')
+}
+else if (num == 5){
+    console.log('%')
+}
+else if (num == 6){
+    console.log('^')
+}
+else if (num == 7){
+    console.log('&')
+}
+else if (num == 8){
+    console.log('*')
+}
+else if (num == 9){
+    console.log('(')
+}
 
 
-// task3
-// let year = prompt('Ввeдите дату')
-// if (year % 400 ===0 ){
-//    console.log(year + ' год - високосный');
-// }
-//
-//  if  (year % 100 === 0 ) {
-//     console.log(year + ' год - невисокосный');
-// }
-// if (year % 4 === 0){
-//     console.log(year + ' год - високосный');
-// }
-// else {
-//     console.log(year + 'год — невисокосный');
-// }
+
+let year = prompt('Ввeдите дату')
+if (year % 400 ===0 ){
+   console.log(year + ' год - високосный');
+}
+
+ if  (year % 100 === 0 ) {
+    console.log(year + ' год - невисокосный');
+}
+if (year % 4 === 0){
+    console.log(year + ' год - високосный');
+}
+else {
+    console.log(year + 'год — невисокосный');
+}
 
 
-// task4
+
 let summa = prompt('Введите сумму')
 let valuta = prompt('Введите валюту (eur, uan, azn)')
 
@@ -84,13 +83,13 @@ else if (valuta = azn){
 }
 
 
-// task5
-// let circle_num = prompt('Введите длину окружности')
-// let square_num = prompt('Введите периметр квадрата')
-//
-// if (circle_num < square_num){
-//     console.log('Может поместиться в кдадрат')
-// }
-// else if (circle_num >= square_num){
-//     console.log('Не может поместиться в квадрат')
-// }
+
+let circle_num = prompt('Введите длину окружности')
+let square_num = prompt('Введите периметр квадрата')
+
+if (circle_num < square_num){
+    console.log('Может поместиться в кдадрат')
+}
+else if (circle_num >= square_num){
+    console.log('Не может поместиться в квадрат')
+}
